a4c2062f1c417b460bfaa61812d68d1ae1a5fd0b
devkotabipin42 <devkotabipin42@.com>
2026-03-25 12:53:29 +0900

    Fix cookie secure settings

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -116,9 +116,11 @@ export const login = async (req, res) => {
         const token = generateToken(account._id, role)
 
         res.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
         res.status(200).json({
             message: 'Login successful',
