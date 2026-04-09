58b216449aae246d5cf4852595de39e2eaaec987
devkotabipin42 <devkotabipin42@.com>
2026-04-09 23:39:39 +0900

    refactor: restructure rendering logic to wrap components in HelmetProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,25 +6,21 @@ import { store, persistor } from './app/store.js'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './app/App.jsx'
+
 if (import.meta.env.PROD) {
     console.log = () => {}
     console.error = () => {}
     console.warn = () => {}
 }
-// ReactDOM.createRoot wrap 
-root.render(
-    <HelmetProvider>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </HelmetProvider>
-)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </HelmetProvider>
   </StrictMode>,
 )
